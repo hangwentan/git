542cca64ea7cf9578942445e753d928040ad97de
haozhikang <2607453529@qq.com>
2018-01-15 15:51:26 +0800

    zk config

--- frontend/web/weibo/config.php
+++ frontend/web/weibo/config.php
@@ -3,4 +3,4 @@ header('Content-Type: text/html; charset=UTF-8');
 
 define( "WB_AKEY" , '2703931843' );
 define( "WB_SKEY" , '62910b5e3e01d8b35697e3e9ba0d30e3' );
-define( "WB_CALLBACK_URL" , 'http://www.orange.com/frontend/web/?r=weibo/callback' );
+define( "WB_CALLBACK_URL" , 'http://127.0.0.1/git/frontend/web/?r=weibo/callback' );
