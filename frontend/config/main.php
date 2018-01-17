a6f5e13536b33e3671e3bb5d9c00408192dd7149
王科 <1114634703@qq.com>
2018-01-17 08:48:29 +0800

    aaa

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,15 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '/'=>"index/home"
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
